3896f6d28e21921615bd5fede1ace2c1b25ac22f
Fena <fe.oorschot@gmail.com>
2023-01-17 01:34:19 -0300

    Feito detalhes do vendedor

--- WebSales/Services/SellerService.cs
+++ WebSales/Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using WebSales.Data;
 using WebSales.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebSales.Services
 {
@@ -23,7 +24,7 @@ namespace WebSales.Services
         }
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(x => x.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(x => x.Id == id);
         }
         public void Remove(int id)
         {
